5dd0607e21a4f533f25ee16455885119cffc6318
Abhay D <a.deshpande012@gmail.com>
2021-09-04 18:58:32 -0700

    Upload model to job dir

--- fleetai-training/fleetai/train_gcp.py
+++ fleetai-training/fleetai/train_gcp.py
@@ -1,5 +1,6 @@
 import shutil
 import os
+import argparse
 
 import yaml
 from google.cloud import storage
@@ -27,14 +28,26 @@ def local_zip_path(args, base=False):
     name = args["agent"]["model_name"] + (".zip" if not base else "")
     return os.path.join(LOCAL_ZIP_DIR, name)
 
+def get_job_dir():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--job-dir", type=str)
+    args, _ = parser.parse_known_args()
+    return args.job_dir
+
+def upload_to_uri(file, uri):
+    blob = uri_to_blob(uri)
+    blob.upload_from_filename(file)
+
 def save_agent(args, agent):
     util.save_agent(LOCAL_MODEL_DIR, args, agent)
     zip_file_local_base = local_zip_path(args, base=True)
     zip_name = os.path.basename(zip_file_local_base) + ".zip"
-    zip_file_remote = os.path.join(args["agent"]["save_dir"], zip_name)
     shutil.make_archive(zip_file_local_base, "zip", LOCAL_MODEL_DIR)
-    blob = uri_to_blob(zip_file_remote)
-    blob.upload_from_filename(zip_file_local_base + ".zip")
+    zip_file_local = zip_file_local_base + ".zip"
+    upload_to_uri(zip_file_local, os.path.join(args["agent"]["save_dir"], zip_name))
+    job_dir = get_job_dir()
+    if job_dir is not None:
+        upload_to_uri(zip_file_local, os.path.join(job_dir, zip_name))
 
 def load_agent(args, agent):
     resume = "resume" in args and args["resume"]
